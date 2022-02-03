033bd5a8a44b8ad4bfc3e4fc01a6208da7df0426
SoshaW <soshalaweerathunge@gmaiol.com>
2022-02-03 13:35:37 +0100

    improved code

---
+++ pose_estimation/src/image_converter.h
@@ -0,0 +1,78 @@
+#include <ros/ros.h>
+#include <image_transport/image_transport.h>
+#include <cv_bridge/cv_bridge.h>
+#include <sensor_msgs/image_encodings.h>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include "ocam_functions.h"
+#include "pose_estimate.h"
+
+static const std::string OPENCV_WINDOW = "Image window";
+
+class bridge
+{
+        ros::NodeHandle nh_;
+        image_transport::ImageTransport it_;
+        image_transport::Subscriber image_sub;
+        arucoMarker estimate;
+
+public:
+
+        cv::Mat img, new_image, resized_image;
+        double c[3];
+        int img_counter = 1;
+
+    bridge()
+    : it_(nh_)
+    {
+        image_sub = it_.subscribe("usb_cam/image_raw", 1 , &bridge::imageCb, this);
+        cv::namedWindow(OPENCV_WINDOW);
+    }
+
+    ~bridge()
+    {
+        cv::destroyWindow(OPENCV_WINDOW);
+    }
+
+    void imageCb(const sensor_msgs::ImageConstPtr& msg)  //function to convert data to cv::Mat
+    {
+        cv::Mat frame;
+        cv_bridge::CvImagePtr cv_ptr;
+        paranomic panaromic;
+
+        try
+        {
+            cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
+        }
+        catch (cv_bridge::Exception& e)
+        {
+            ROS_ERROR("cv_bridge exceprion: %s",e.what());
+            return;
+        }
+
+        img = cv_ptr->image;
+
+        frame = panaromic.slice(img, c);
+
+        new_image = estimate.pose_marker(frame, c);
+        //new_image = estimate.pose_board(frame, c);
+
+       /* double width = new_image.size().width;
+        double height = new_image.size().height;
+
+        cv::resize(new_image, resized_image, cv::Size(width, height));*/
+
+        cv::imshow(OPENCV_WINDOW, new_image);
+
+        int k = cv::waitKey(1);
+
+        if (k%256 == 32)
+        {
+            std::string name = "/home/tue-me-minicar-laptop-02/internship/camera_calibration/camera_02/data4_512p_90_60/open_cv_img" + std::to_string(img_counter) + ".png" ;
+            cv::imwrite(name, new_image);
+            img_counter++;
+        }
+
+    }
+
+};
---
+++ pose_estimation/src/pose_estimate.h
@@ -0,0 +1,222 @@
+#ifndef POSE_ESTIMATE_H
+#define POSE_ESTIMATE_H
+
+#include <opencv2/aruco.hpp>
+#include <opencv2/core.hpp>
+#include <opencv2/opencv.hpp>
+#include <ros/ros.h>
+#include "geometry_msgs/Pose.h"
+#include "tf/transform_broadcaster.h"
+
+class arucoMarker
+{
+
+private:
+
+    cv::Mat camera_matrix = cv::Mat::eye(3, 3, CV_64FC1);
+    //512p 60_45
+    cv::Mat distcoefs = (cv::Mat_<double>(5 , 1) << -0.427771206145061, 0.633996195883629, -0.045465437991085, 0.039523036767817, -0.404909636477268);
+    //768p 90_60
+    //cv::Mat distcoefs = (cv::Mat_<double>(5 , 1) << 0.00652234, -0.00916935, -0.04434304, 0.00916721, -0.0195544);
+    cv::Ptr<cv::aruco::Dictionary> Dictionary = cv::aruco::getPredefinedDictionary(cv::aruco::DICT_4X4_250);
+
+    ros::NodeHandle nh;
+    ros::Publisher pub;
+    ros::Subscriber abs_cam;
+    ros::Subscriber abs_mrkr;
+
+    geometry_msgs::Pose position;
+
+    tf::Transform transform;
+    tf::Transform transform2;
+    tf::Quaternion q;
+
+    int blur_window_size;
+    double cam_x, cam_y, cam_z, mrkr_x, mrkr_y, mrkr_z, x_, y_, z_, mod, error, accuracy;
+    double x, y, z, cp_x, cp_y, cp_z, p_x, p_y, p_z, x_tr, y_tr, z_tr, x_pr, y_pr, abs_x, abs_y, abs_z, abs_dist, est_dist;
+
+public:
+
+    arucoMarker()
+    {
+        //resolution 512 60_45
+        camera_matrix.at<double>(0,0) = 4.907252377393949e+02;
+        camera_matrix.at<double>(0,2) = 1.358011912842060e+02;
+        camera_matrix.at<double>(1,1) = 4.893497005137714e+02;
+        camera_matrix.at<double>(1,2) = 1.228809311531833e+02;
+
+        //resolution 768 90-60
+        /*camera_matrix.at<double>(0,0) = 441.6957675;
+        camera_matrix.at<double>(0,2) = 385.65077995;
+        camera_matrix.at<double>(1,1) = 450.91648568;
+        camera_matrix.at<double>(1,2) = 143.78493894;
+
+        //resolution 1024
+        /*camera_matrix.at<double>(0,0) = 590.619400577650;
+        camera_matrix.at<double>(0,2) = 519.693135481507;
+        camera_matrix.at<double>(1,1) = 598.235920789867;
+        camera_matrix.at<double>(1,2) = 285.442343211893;*/
+
+        blur_window_size = 7;
+
+        pub = nh.advertise<geometry_msgs::Pose>("pose",5);
+    }
+
+
+    cv::Mat pose_marker(cv::Mat image, double c[3], std::vector<int> id = {0}, double marker_len = 0.1)
+    {
+        cv::Mat new_image, gray_img;
+        image.copyTo(new_image);
+        cv::cvtColor(new_image, gray_img, cv::COLOR_BGR2GRAY);
+
+        std::vector<int> ids, ids_to_est;
+        std::vector<std::vector<cv::Point2f>> corners, corners_to_est;
+
+        int k = 0; //counter for the array of markers to estimate the pose
+
+        cv::aruco::detectMarkers(gray_img, Dictionary, corners, ids);
+
+        if (ids.size() > 0)
+        {
+            for (int i = 0; i < id.size(); i++)
+            {
+                for (int j = 0; j < ids.size(); j++)
+                {
+                    if (id[i] == ids[j])
+                    {
+                        ids_to_est[k] = ids[j];
+                        corners_to_est[k] = corners[j];
+                        k++;
+                    }
+                }
+            }
+
+            cv::aruco::drawDetectedMarkers(new_image, corners_to_est, ids_to_est);
+            std::vector<cv::Vec3d> rvecs, tvecs;
+            cv::aruco::estimatePoseSingleMarkers(corners, marker_len, camera_matrix, distcoefs, rvecs, tvecs);
+
+            for (int i = 0; i < ids.size(); i++ )
+            {
+
+                cv::aruco::drawAxis(new_image, camera_matrix, distcoefs, rvecs[i], tvecs[i], 0.1); //0.1 length of the drawn axis
+                broadcast(rvecs[i], tvecs[i], c);
+            }
+
+        }
+
+        return new_image;
+    }
+
+    cv::Mat pose_board(cv::Mat image, double c[3])
+    {
+        cv::Mat new_image, grey_image;
+        image.copyTo(new_image);
+        cv::cvtColor(new_image, grey_image, cv::COLOR_BGR2GRAY);
+
+        cv::Ptr<cv::aruco::GridBoard> board = cv::aruco::GridBoard::create(2, 2, 0.04, 0.01, Dictionary);
+
+        std::vector<int> ids;
+        std::vector<std::vector<cv::Point2f>> corners;
+
+        cv::aruco::detectMarkers(grey_image, Dictionary, corners, ids);
+
+        if(ids.size() > 0)
+        {
+            cv::aruco::drawDetectedMarkers(new_image, corners, ids);
+            cv::Vec3d rvecs, tvecs;
+            int valid = cv::aruco::estimatePoseBoard(corners, ids, board, camera_matrix, distcoefs, rvecs, tvecs);
+
+            if (valid > 0)
+            {
+                cv::aruco::drawAxis(new_image, camera_matrix, distcoefs, rvecs, tvecs, 0.1);
+                broadcast(rvecs, tvecs, c);
+            }
+
+        }
+
+        return new_image;
+
+    }
+
+    void update_cam_pose(const geometry_msgs::PoseStamped& msg)
+    {
+        cam_x = msg.pose.position.y;
+        cam_y = msg.pose.position.x;
+        cam_z = msg.pose.position.z;
+    }
+
+    void update_mrkr_pose(const geometry_msgs::PoseStamped& msg)
+    {
+        mrkr_x = msg.pose.position.y;
+        mrkr_y = msg.pose.position.x;
+        mrkr_z = msg.pose.position.z;
+
+    }
+
+    void broadcast(cv::Vec3d rvecs, cv::Vec3d tvecs, double c[3])
+    {
+        cp_x = c[0];
+        cp_y = c[1];
+        cp_z = c[2];
+        
+        static tf::TransformBroadcaster br;      
+        
+        x_ = tvecs[0];
+        y_ = tvecs[1];
+        z_ = tvecs[2];
+
+        x_pr = x_/z_;
+        y_pr = y_/z_;
+
+        x_tr = cp_y*x_pr + cp_x*cp_z*y_pr + cp_x;
+        y_tr = -cp_x*x_pr + cp_y*cp_z*y_pr + cp_y;
+        z_tr = -(cp_y*cp_y + cp_x*cp_x)*y_ + cp_z;
+
+        mod = sqrt(x_tr*x_tr + y_tr*y_tr + z_tr*z_tr);
+
+        x = x_tr/mod;
+        y = y_tr/mod;
+        z = z_tr/mod;
+        
+        p_x = x*z_;
+        p_y = y*z_;
+        p_z = z*z_;
+
+        est_dist = sqrt((p_x*p_x + p_y*p_y + p_z*p_z));
+
+        abs_cam = nh.subscribe("/vrpn_client_node/fisheyed_camera/pose", 1000, &arucoMarker::update_cam_pose, this);
+        abs_mrkr = nh.subscribe("/vrpn_client_node/fisheyed_marker/pose", 1000, &arucoMarker::update_mrkr_pose, this);
+
+        transform.setOrigin(tf::Vector3(p_x, p_y, p_z));
+        q.setRPY(rvecs[0], rvecs[1], rvecs[2]);
+        q.normalize();
+        transform.setRotation(q);
+
+
+        br.sendTransform(tf::StampedTransform(transform, ros::Time::now(), "fisheyed_camera", "fisheyed_marker_pred"));
+
+        if (abs_cam && abs_mrkr)
+        {
+            abs_y = -(mrkr_x - cam_x);
+            abs_x = mrkr_y - cam_y;
+            abs_z = mrkr_z - cam_z;
+
+            //abs_dist = sqrt((abs_x*abs_x + abs_y*abs_y + abs_z*abs_z));
+
+<<<<<<< HEAD
+            error= abs(abs_x - p_x);
+            accuracy = 100 - abs(error/abs_x)*100;
+
+           ROS_INFO_STREAM("Error x: "<< error << " Accuracy: " << accuracy << " Absolute_dist :" << abs_dist);
+=======
+            error = abs(abs_dist - est_dist);
+            accuracy = 100 - (error/abs_dist)*100;
+
+           ROS_INFO_STREAM("Error: "<< error << " Accuracy: " << accuracy << " Absolute_dist :" << abs_dist);
+>>>>>>> 9afa858e15106e01c39c95e841a4142e3bbe0d75
+        }
+    }
+
+};
+
+#endif // POSE_ESTIMATE_H
